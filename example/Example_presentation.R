d7b86056599babea457e8cd386a2bde9c4b96225
Burg <l.l.j.van_der_burg@lumc.nl>
2025-06-12 09:46:26 +0200

    Script for Opal presentation

---
+++ example/Example_presentation.R
@@ -0,0 +1,52 @@
+# Start -------------------------------------------------------------------
+## Script to show how Opal and Mica work
+#
+## written by: Lars van der Burg
+## written on: 27-05-2025
+
+
+# Initiations -------------------------------------------------------------
+library(tidyverse)
+library(keyring)
+library(opalr)
+library(diffdf)
+library(OpalLUMCADM)
+
+
+## Opal -------------------------------------------------------------------
+opal_url = "https://opal-tst92.clinicalresearch.nl"
+opal_token = keyring::key_get("token_tst_clinicalresearch")
+
+opal = opalr::opal.login(url = opal_url, token = opal_token)
+
+projname = "Example"
+tablename = "Example_data"
+
+
+## Data -------------------------------------------------------------------
+load("example/FAKE_datafile.RData"); datafile
+load("example/FAKE_var.RData"); var
+load("example/FAKE_cat.RData"); cat
+
+
+# Export ------------------------------------------------------------------
+OpalLUMCADM::write_table_R2opal(opal = opal, projname = projname, tablename = tablename,
+                                datafile = datafile, var = var, cat = cat,
+                                ent = "Participant", action = "write")
+
+
+# Taxonomies --------------------------------------------------------------
+annotations = openxlsx::read.xlsx("Mica/Taxonomy_example.xlsx", sheet = "Annotations")
+opalr::opal.annotate(opal = opal, datasource = projname, table = tablename, annotations = annotations)
+
+
+# Import ------------------------------------------------------------------
+datadict = OpalLUMCADM::import_table_opal2R(opal = opal, projname = projname, tablename = tablename)
+datadict$datafile4copy
+
+
+# View --------------------------------------------------------------------
+OpalLUMCADM::make_opal_view(opal = opal, projname = projname, tablename = tablename,
+                            opal_view = opal, projname_view = projname, tablename_view = paste0(tablename, "_view"),
+                            var = var, cat = cat, ent = "Participant", EntityFilter = c("group" = "A"), report_path = FALSE)
+
