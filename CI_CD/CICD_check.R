21b1687f4a443e65a50b73f479a4e52b8359932d
Burg <l.l.j.van_der_burg@lumc.nl>
2025-08-18 13:49:41 +0200

    update comment-out keyring

--- CI_CD/CICD_check.R
+++ CI_CD/CICD_check.R
@@ -62,17 +62,17 @@ if(server == "demo"){
   opal_url = "https://opal-demo.obiba.org"; opal_username = "administrator"; opal_password = "password"; projname = "TESTING"
   opal_token = NULL
 
-} else if(server == "acc"){
-  opal_url = "https://opal-acc93.clinicalresearch.nl"; opal_token = keyring::key_get("token_acc_clinicalresearch"); projname = "TEST_LARS"
-  opal_username = opal_password = NULL
-
-  source("CI_CD/CICD_procedure.R")
-
-} else if(server == "prod"){
-  opal_url = "https://opal.clinicalresearch.nl"; opal_token = keyring::key_get("token_opal_clinicalresearch"); projname = "TEST_LARS"
-  opal_username = opal_password = NULL
-
-  source("CI_CD/CICD_procedure.R")
+# } else if(server == "acc"){
+#   opal_url = "https://opal-acc93.clinicalresearch.nl"; opal_token = keyring::key_get("token_acc_clinicalresearch"); projname = "TEST_LARS"
+#   opal_username = opal_password = NULL
+#
+#   source("CI_CD/CICD_procedure.R")
+#
+# } else if(server == "prod"){
+#   opal_url = "https://opal.clinicalresearch.nl"; opal_token = keyring::key_get("token_opal_clinicalresearch"); projname = "TEST_LARS"
+#   opal_username = opal_password = NULL
+#
+#   source("CI_CD/CICD_procedure.R")
 
 }
 
