97e4e4f085844c90e0ff277bf4b3c61a7b6ff616
Tom Kuipers <t.b.kuipers@lumc.nl>
2025-09-08 14:09:11 +0200

    add load message

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+
+#' Loading message package OpalLUMCADM
+#'
+#' @export
+
+.onLoad <- function(libname, pkgname){
+  ## https://patorjk.com/software/taag/#p=display&f=Big&t=OpalLUMCADM
+  packageStartupMessage(
+    paste0(
+      "
+   ____              _ _     _    _ __  __  _____          _____  __  __ 
+  / __ \\            | | |   | |  | |  \\/  |/ ____|   /\\   |  __ \\|  \\/  |
+ | |  | |____   ____| | |   | |  | | \\  / | |       /  \\  | |  | | \\  / |
+ | |  | |  _ \\ / _  | | |   | |  | | |\\/| | |      / /\\ \\ | |  | | |\\/| |
+ | |__| | |_) | (_| | | |___| |__| | |  | | |____ / ____ \\| |__| | |  | |
+  \\____/|  __/ \\____|_|______\\____/|_|  |_|\\_____/_/    \\_\\_____/|_|  |_|
+        | |                                                              
+        |_|                                                              
+      "
+    )
+  )
+}
