5af579c012f43b59869bcc9c34d5e8b830e3c722
Tom Kuipers <t.b.kuipers@lumc.nl>
2025-09-18 16:35:21 +0200

    add methid

--- R/import_export.R
+++ R/import_export.R
@@ -40,18 +40,52 @@ adm.table_get <- function(opal, project, table, ...) {
 }
 
 
+#' Function to set force & overwrite
+#'
+#' @param method String: write, update or overwrite
+#'
+#' @import opalr dplyr
+#' 
+#' @return 
+#'
+#' @export
+
+## Retrieved from import_table_opal2R()
+adm.set_method <- function(method) {
+  ## Set method
+  if(method == "write"){
+    method = c(force = FALSE, overwrite = FALSE)
+  } else if(method == "update"){
+    method = c(force = TRUE, overwrite = FALSE)
+  } else if(method == "overwrite"){
+    method = c(force = TRUE, overwrite = TRUE)
+  } else {
+    stop("The method argument can only have three options: write, update or overwrite.")
+  }
+  
+  return(method)
+}
+
+
 #' Function to save a table from R to Opal
 #'
 #' @param opal a working opalr::opal_login
 #' @param project Origin opal project name
 #' @param table Origin opal table name
+#' @param datafile data dataframe
+#' @param variables variables dataframe
+#' @param categories categories dataframe
+#' @param method String: write, update or overwrite
 #'
 #' @import opalr dplyr
 #' 
 #' @export
 
 ## Retrieved from write_table_R2opal()
-adm.table_save <- function(opal, project, table, datafile, variables, categories = NULL, ...) {
+adm.table_save <- function(opal, project, table, datafile, variables, categories = NULL, method,...) {
+  ## Set method
+  method <- adm.set_method(method = method)
+  
   ## Apply dictionary if variables are present
   if (!missing(variables)) {
     datafile <- dictionary.apply(
@@ -67,7 +101,17 @@ adm.table_save <- function(opal, project, table, datafile, variables, categories
     project = project,
     table = table,
     tibble = datafile,
+    force = method["force"],
+    overwrite = method["overwrite"],
     ...
+  )
+  
+  ## Remove user own permissions (if you can make a table, you have to have project rights)
+  opal.table_perm_delete(
+    opal = opal,
+    project = projname,
+    table = tablename,
+    subject = opal$username
   )
 }
 
