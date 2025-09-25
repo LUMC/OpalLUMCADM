3e8ba28c2d5957ae7c4775058fe3e3136169e5fe
Tom Kuipers <t.b.kuipers@lumc.nl>
2025-09-25 15:37:56 +0200

    start setting cicd

---
+++ cicd/install_packages.R
@@ -0,0 +1,10 @@
+
+## Install required packages
+install.packages("opalr")
+install.packages("dplyr")
+install.packages("rlang")
+install.packages("diffdf")
+install.packages("openxlsx")
+install.packages("rtres")
+install.packages("stringr")
+install.packages("lubridate")
