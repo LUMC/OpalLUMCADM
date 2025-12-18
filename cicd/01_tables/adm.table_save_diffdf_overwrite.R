cd6da848408db73dd2b561b773e4e01ddd8df467
Tom Kuipers <t.b.kuipers@lumc.nl>
2025-12-18 11:36:34 +0100

    add test

---
+++ cicd/01_tables/adm.table_save_diffdf_overwrite.R
@@ -0,0 +1,46 @@
+
+## Load libraries
+library(opalr)
+library(dplyr)
+library(OpalLUMCADM)
+
+## Set opal-demo login
+opal <- opal.login(
+  username = "administrator",
+  password = "password",
+  url = "https://opal-demo.obiba.org/"
+)
+
+## Create dataframes
+df <- tibble(
+  id = 1:3,
+  a = c("A", "B", "C"),
+  b = c(1, 2, 3)
+)
+
+## Save dataframe
+findings <- adm.table_save_diffdf(
+  opal = opal,
+  project = "CNSIM",
+  table = "CNSIM_TEST",
+  datafile = df,
+  method = "overwrite"
+)
+
+## Run check for success
+test_that("success", {
+  output <- capture.output(
+    adm.table_save_diffdf(
+      opal = opal,
+      project = "CNSIM",
+      table = "CNSIM_TEST",
+      datafile = df,
+      method = "overwrite"
+    )
+  )
+  
+  expect_equal(
+    object = output,
+    expected = c("$datafile", "No issues were found!", "", "$variables", "No issues were found!", "")
+  )
+})
